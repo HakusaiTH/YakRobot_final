a82affff00dd42d65f133e32e1bd73ac6d64ae98
Fujiwara Aoyama <104154862+HakusaiTH@users.noreply.github.com>
2023-12-13 20:56:52 +0700

    Update firestore_upload.py

--- python/firestore_upload.py
+++ python/firestore_upload.py
@@ -2,8 +2,8 @@ import os
 from firebase_admin import credentials, initialize_app, storage
 
 def upload_mp3_to_firebase(file_path, storage_path="audio"):
-    cred = credentials.Certificate('D:\\sanbot_final\\pythonbackend\\my-robot-9fdff-firebase-adminsdk-37upn-709ad75796.json')
-    initialize_app(cred, {'storageBucket': 'my-robot-9fdff.appspot.com'})
+    cred = credentials.Certificate('... .json')
+    initialize_app(cred, {'storageBucket': '... appspot.com'})
 
     bucket = storage.bucket()
 
@@ -14,10 +14,3 @@ def upload_mp3_to_firebase(file_path, storage_path="audio"):
     blob.upload_from_filename(file_path)
 
     print(f"File {file_name} uploaded to Firebase Storage at {destination_path}")
-
-# Example usage
-'''
-mp3_file_path = "output.mp3"
-upload_mp3_to_firebase(mp3_file_path)
-
-'''
